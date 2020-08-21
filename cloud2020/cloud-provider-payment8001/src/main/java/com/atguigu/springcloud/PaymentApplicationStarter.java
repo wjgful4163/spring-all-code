08ad2dd330bfdbb25be30a44878c54f52caa3ba4
wjgful <wjg20144128@163.com>
2020-08-21 09:57:23 +0800

    支付简单Demo

--- cloud2020/cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymentApplicationStarter.java
+++ cloud2020/cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymentApplicationStarter.java
@@ -1,6 +1,5 @@
 package com.atguigu.springcloud;
 
-import com.sun.glass.ui.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- cloud2020/cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/service/impl/PaymentServiceImpl.java
+++ cloud2020/cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/service/impl/PaymentServiceImpl.java
@@ -4,6 +4,7 @@ import com.atguigu.springcloud.dao.PaymentDao;
 import com.atguigu.springcloud.entities.CommResult;
 import com.atguigu.springcloud.entities.Payment;
 import com.atguigu.springcloud.service.PaymentService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,11 +12,13 @@ import org.springframework.stereotype.Service;
  * @author: wjgful
  */
 @Service
+@Slf4j
 public class PaymentServiceImpl implements PaymentService {
     @Autowired
     private PaymentDao paymentDao;
     @Override
     public CommResult create(Payment payment) {
+        log.info("创建数据...");
         int i= paymentDao.create(payment);
         if(i>0){
          return new CommResult(200,"插入数据成功！");
@@ -25,9 +28,10 @@ public class PaymentServiceImpl implements PaymentService {
 
     @Override
     public CommResult<Payment> getPaymentById(long id) {
+        log.info("获取数据，id={}",id);
         Payment payment = paymentDao.getPaymentById(id);
         if(payment!=null){
-            return new CommResult<Payment>(200,"查询数据成功",payment);
+            return new CommResult<>(200,"查询数据成功",payment);
         }
         return new CommResult<>(444,"没有对应数据"+id,null);
     }
